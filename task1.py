d830bddd541cb93d7ede43687437213f5578108c
Fachii Zasha <zashafachii@gmail.com>
2021-12-13 22:49:17 +0100

    Modified Program
    Ask for user name
    Ask for user age
    Check if name is letters only and age is digit only
    Print out the result if validation is passed

--- task1.py
+++ task1.py
@@ -1,8 +1,22 @@
-#To allow the user input name
-name = input("What is your name?: ")
+"""Algorithm Structure Used
+    Input fields: Name and Age
+    Expected Output: so you are {Name} and {Age} years old. Nice to meet
+    Ask for name
+    Ask for age
+    Validate age as a number
+    Output
+"""
 
-#To allow the user input age
-age = int(input("How old are you?: "))
+print("Hello how are you doing??\n")
 
-#To output the user's name and age
-print("Your name is "+ name + " and you're " + str(age) + " years old." )
+condition = True
+while condition:
+    name = input("can I please know your name?\n")
+    age = input("can I please know your age?\n")
+
+    if name.isalpha() & age.isdigit():
+        output = f"\nso you are {name} and you're {age} years old. Nice to meet you"
+        print(output)
+        condition = False
+    else:
+        print("Invalid name Or Age, Please pass in a number for the age and letter only for the name\n")
